d500f518199c7d370d190e3bc20c1a4c19a0da87
iain barnett <iainspeed@gmail.com>
2018-07-23 17:44:02 +0100

    Added some scripts.

---
+++ Scripts/jslint.rb
@@ -0,0 +1,19 @@
+#!/usr/bin/env ruby
+require 'pathname'
+
+original = Pathname(ENV["BB_DOC_PATH"]).read
+
+
+require 'tempfile'
+
+fh = Tempfile.new ["jslint-", ".js"]
+fh.write original
+fh.rewind
+begin
+  pattern = '(?P<file>.+?):[[:space:]]line[[:space:]](?P<line>[[:digit:]]+),[[:space:]]col[[:space:]](?P<col>[[:digit:]]+),[[:space:]]+(?P<msg>.*)$'
+  
+  cmd = %Q!/usr/local/bin/jshint #{fh.path} | bbresults --pattern '#{pattern}'!
+  `#{cmd}`
+rescue => e
+  warn e
+end
---
+++ Scripts/ruby-version.rb
@@ -0,0 +1,3 @@
+#!/usr/bin/env ruby
+
+print RUBY_VERSION
